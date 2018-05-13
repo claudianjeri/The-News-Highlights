13d9559cc71a7e9e1b7b766f77f8a88bf65161a0
claudia njeri <claudianjeri04@gmail.com>
2018-05-13 21:32:19 +0300

    processing results

--- app/request.py
+++ app/request.py
@@ -28,3 +28,29 @@ def get_sources(category):#this function takes category as an argument
 
 
     return source_results #then we return the list.
+
+def process_results(source_list):#takes in a list of dictionaries
+    
+  '''
+    function that processes the news results and transform them to a list of objects
+    Args:
+        source_list: A list of dictionaries that contain news details
+    Returns:
+        source_results: Alist of news source objects
+    '''
+    source_results = [] #create an empty list. It will store source objects
+    for source_item in source_list: #loops through the list of dictionary and passes in keys using the get method, so that I can get the values.
+        id = source_item.get('id')
+        name = source_item.get('name')
+        description = source_item.get('description')
+        url = source_item.get('url')
+        category = source_item.get('category')
+        language = source_item.get('language')
+        country = source_item.get('country')
+
+        if id: ()#checks if the object has an id. If it does, its added to the list.
+            source_object = Source(id,name,description,url,category,language,country)
+
+            source_results.append(source_object)
+
+    return source_results
