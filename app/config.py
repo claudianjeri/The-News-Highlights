5483b19c69b9b66673ac27a8fb5461aec514e6ad
claudia njeri <claudianjeri04@gmail.com>
2018-05-13 21:22:08 +0300

    Defined the first response

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ class Config:
     NEWS_API_KEY = '8a48624dfbfc41d8a7009f960cccd473'
 
     #my source base url
-    SOURCE_API_BASE_URL = 'https://newsapi.org/v2/sources?category={}&apiKey=8a48624dfbfc41d8a7009f960cccd473'
+    SOURCE_API_BASE_URL = 'https://newsapi.org/v2/sources?&apiKey=8a48624dfbfc41d8a7009f960cccd473'
 
 class ProdConfig(Config):
 
--- app/request.py
+++ app/request.py
@@ -7,4 +7,24 @@ Source = news.Source
 api_key = app.config['NEWS_API_KEY']#we get the api key for the requests.
 base_url = app.config['SOURCE_API_BASE_URL']#getting the base url 
 
-# def get_sources(category):
+def get_sources(category):#this function takes category as an argument
+
+     '''
+    Function that gets the json response to our url request
+
+    '''
+
+    get_source_url = base_url.format(category,api_key) #.format method  on the base_url and pass in the movie category and the api_key
+    #our final URL.
+    with urllib.request.urlopen(get_source_url) as url:#sends the request as a url
+        get_source_data = url.read()#read() reads the response and stores it.
+        get_source_response = json.loads(get_source_data)#the response is in JSON format, this line converts it to a python dictionary.
+
+        source_results = None
+
+        if get_source_response['sources']:#checks if the response has any results
+            source_results_list = get_source_response['sources']
+            source_results = process_results(source_results_list)
+
+
+    return source_results #then we return the list.
