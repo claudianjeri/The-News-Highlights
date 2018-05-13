eb129f82ab9748623f4048291402832f42c1a4b1
claudia njeri <claudianjeri04@gmail.com>
2018-05-13 20:59:28 +0300

    Creating the first api call

--- app/news_test.py
+++ app/news_test.py
@@ -1,7 +1,7 @@
 import unittest #import unittest module 
 from .models import Source #import the news module
 
-News = news.Source #getting news class 
+Source = news.Source #getting news class 
 
 class NewsTest(unittest.Testcase): #created a subclass and defined the test case.
 
@@ -12,7 +12,7 @@ class NewsTest(unittest.Testcase): #created a subclass and defined the test case
         self.new_news = Source()
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.new_news, News))
+        self.assertTrue(isinstance(self.new_news, Source))
 
 if __name__ == '__main__':
     unittest.main()
--- app/request.py
+++ app/request.py
@@ -1,3 +1,10 @@
 from app import app #importing app instance
+import urllib.request, json #imported urllib.request module that will help in the creation of the API URL and sends a request to json modules
+from .models import news
 
-api_key = app.config('NEWS_API_KEY')#we get the api key for the requests.
+Source = news.Source
+
+api_key = app.config['NEWS_API_KEY']#we get the api key for the requests.
+base_url = app.config['SOURCE_API_BASE_URL']#getting the base url 
+
+# def get_sources(category):
