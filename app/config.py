36e8607f2a983bd1eaf3c9aaba20a7090c5af302
claudia njeri <claudianjeri04@gmail.com>
2018-05-27 00:47:55 +0300

    changed the articles base url

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ class Config:
     SOURCE_API_BASE_URL = 'https://newsapi.org/v2/sources?&category={}&language=en&apiKey=8a48624dfbfc41d8a7009f960cccd473'
 
     #my articles base url
-    ARTICLES_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?sources=cnn&apiKey=8a48624dfbfc41d8a7009f960cccd473'
+    ARTICLES_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey=8a48624dfbfc41d8a7009f960cccd473'
 class ProdConfig(Config):
 
     pass
